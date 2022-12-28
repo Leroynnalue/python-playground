d00c6cddb57e5543577f2dcc783729e0ed03a92b
ALIENROY <leroynnalue65@gmail.com>
2022-12-28 22:39:24 +0100

    Edit: + Largest Number

---
+++ basic/largestNumber.py
@@ -0,0 +1,8 @@
+numbers = [1,3,56,904,5,2,103]
+largest_number = -99
+
+for number in numbers:
+    if number > largest_number:
+        largest_number = number
+
+print("Largest Number:",largest_number)
